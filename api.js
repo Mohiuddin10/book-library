cbe0188534337ea77d1fac9b605a8bea2e2a6f5b
Mohiuddin10 <sj.ctg6737@gmail.com>
2021-12-08 20:04:54 +0600

    secound commit

---
+++ api.js
@@ -0,0 +1,40 @@
+const searchBook = () => {
+    const searchFeild = document.getElementById("input-text");
+    const searchText = searchFeild.value;
+    console.log(searchText);
+    searchFeild.value = "";
+
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayBook(data.docs))
+}
+
+const displayBook = (books) => {
+    const searchResult = document.getElementById("search-result");
+    const count = document.getElementById("result-count");
+    searchResult.textContent = '';
+    if (books.length == 0) {
+        const h2 = document.createElement('h2');
+        h2.innerText = 'sorry no result found';
+        searchResult.appendChild(h2);
+        count.textContent = '';
+    }
+    else {
+        books.forEach(book => {
+            console.log(book);
+            count.textContent = `Total search result found: ${books.length}`;
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card">
+            <img style="border-radius: 17px; height: 350px; width: auto;" class="img-thumbnail" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${book.title}</h5>
+            <p class="card-text">author_name: ${book.author_name}</p>
+            <p class="card-text"><small class="text-muted">first_publish_year: ${book.first_publish_year}</small></p>
+            </div>`;
+            searchResult.appendChild(div);
+        })
+    }
+}
